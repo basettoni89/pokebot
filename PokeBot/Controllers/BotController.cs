675d3de70cce3277f618f3ff7c933326c987fb4a
Davide Mortara <dmortara@alteanet.it>
2019-05-26 19:48:12 +0200

    Added sendMessage response

--- PokeBot/Controllers/BotController.cs
+++ PokeBot/Controllers/BotController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PokeBot.Controllers
@@ -10,10 +14,43 @@ namespace PokeBot.Controllers
     [ApiController]
     public class BotController : ControllerBase
     {
-        [HttpPost]
-        public ActionResult<string> OnNewMessage()
+        private const string BOT_TOKEN = "745900249:AAGN4eqAhz6NLEn5fh8GZLywacmaH3UMgI8";
+
+        private readonly ILogger<BotController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public BotController(ILogger<BotController> logger,
+            IHttpClientFactory clientFactory)
         {
-            return "I'm alive!";
+            this._logger = logger;
+            this._clientFactory = clientFactory;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> OnNewMessage(JObject message)
+        {
+            _logger.LogDebug($"Received [{message}]");
+
+            string content = (string) message["text"];
+            int chatId = (int)message["chat"]["id"];
+
+            if(!string.IsNullOrEmpty(content) && chatId > 0)
+            {
+                var response = JObject.FromObject(
+                    new
+                    {
+                        chat = chatId,
+                        text = $"{content}. Ok, copy!"
+                    });
+
+                var responseResult = await _clientFactory.CreateClient()
+                    .PostAsync($"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+                        new StringContent(response.ToString(), Encoding.UTF8, "application/json"));
+
+                _logger.LogDebug($"SendMessage result [{responseResult.StatusCode}]");
+            }
+
+            return Ok();
         }
     }
 }
