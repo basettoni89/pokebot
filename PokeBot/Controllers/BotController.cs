532e4f6aec63f466657ef221eff3ff336b2015bc
Davide Mortara <dmortara@alteanet.it>
2019-05-26 10:44:19 +0200

    Added base webhook call

---
+++ PokeBot/Controllers/BotController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeBot.Controllers
+{
+    [Route("745900249:AAGN4eqAhz6NLEn5fh8GZLywacmaH3UMgI8/new-message")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<string> OnNewMessage()
+        {
+            return "I'm alive!";
+        }
+    }
+}
